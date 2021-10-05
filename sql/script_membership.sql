aa7edfff1c63f93da1d482db51c1fcdaf2216c32
mouloukin <raphael.mouloukin.etu@univ-lille.fr>
2021-10-05 15:35:19 +0100

    ADD - Views, manage data with db etc...

---
+++ sql/script_membership.sql
@@ -0,0 +1,7 @@
+﻿insert into dbo.MembershipTypes values (1, 0, 0, 0, 'Paiement');
+insert into dbo.MembershipTypes values (2, 30, 1, 10, 'Mensuel');
+insert into dbo.MembershipTypes values (3, 90, 3, 15, 'Trimestriel');
+insert into dbo.MembershipTypes values (4, 300, 12, 20, 'Annuel');
+
+insert into dbo.Customers values (1, 'Ja Morant', 'false', 1);
+insert into dbo.Customers values (2, 'Jaren Jackson Jr', 'true', 3);
